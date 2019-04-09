4c40edb9df3b98211bf61517a75fd4b1f1344d24
Matt Barr <matthew.barr@allegion.com>
2019-04-09 10:34:48 -0400

    Updated v3 to newest version of gradle

--- Android Accessories/Basic Communication Demo - OpenAccessory Framework/App Source Code/v3.x/gen/com/microchip/android/BasicAccessoryDemo_API12/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.microchip.android.BasicAccessoryDemo_API12;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
